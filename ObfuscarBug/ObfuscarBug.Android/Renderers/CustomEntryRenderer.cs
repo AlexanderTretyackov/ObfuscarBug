a0a107d0ced5c88e068e322f9747fff40c207e4f
ObfuscarBug <tretyackov@biogenom.ru>
2022-11-14 19:35:46 +0500

    init commit

---
+++ ObfuscarBug/ObfuscarBug.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,20 @@
+﻿using Android.Content;
+using ObfuscarBug.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace ObfuscarBug.Droid.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}
